d7ec835f1a896e04f29a8ca6cc22e11484484e87
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-07-15 09:34:58 +0530

    array objects

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,23 @@
-var circum = function (diameter) {
-    return diameter * Math.PI;
+// arrays 
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+//names[0] = 4;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ['ken', 4, 'chu-li', 8, 9];
+mixed[1] = 'gtm';
+console.log(mixed);
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circum(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+ninja = {
+    name: 'gtm',
+    belt: 'brown',
+    age: 25
+};
+console.log(ninja);
--- sandbox.ts
+++ sandbox.ts
@@ -1,7 +1,40 @@
+// arrays 
+let names = ['luigi', 'mario', 'yoshi'];
 
-const circum = (diameter: number) =>{
-    return diameter * Math.PI;
+names.push('toad');
+//names[0] = 4;
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+
+
+let mixed = ['ken', 4, 'chu-li', 8, 9];
+mixed[1] = 'gtm';
+
+console.log(mixed);
+
+
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+
+ninja = {
+    name: 'gtm',
+    belt: 'brown',
+    age: 25
 }
 
 
-console.log(circum(7.5));
+
+console.log(ninja);
+
+
+
+
