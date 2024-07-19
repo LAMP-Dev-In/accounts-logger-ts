8454b49efd45809dbd61fc21860e4b0394393016
LAMP-Dev-In <lamp.developer.in@gmail.com>
2024-07-19 13:58:05 +0530

    Function type

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('test config 1234');
+var greet;
+//greet = 'hello';
+greet = function () {
+    console.log('Hello Gtm!');
+};
+greet();
+var add = function (a, b, c) {
+    if (c === void 0) { c = 12; }
+    console.log(a + b);
+    console.log(c);
+};
+add(2, 8, 'KLM');
+var minus = function (a, b) {
+    return (a - b);
+};
+var result = minus(10, 7);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,27 @@
-console.log('test config 1234');
+let greet: Function;
+
+
+//greet = 'hello';
+
+greet = () => {
+    console.log('Hello Gtm!');
+}
+
+greet();
+
+const add = (a: number, b: number, c: number | string = 12): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(2, 8, 'KLM');
+
+
+const minus = (a: number, b:number) => {
+    return (a - b);
+}
+
+let result = minus(10, 7);
+
+console.log(result);
+
